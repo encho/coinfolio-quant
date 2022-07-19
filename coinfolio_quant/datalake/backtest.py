0bd09cc0f91f3fae33bc60b85a425bf20cd167d5
Lorenzo Bertolini <lorenzo@vento.io>
2022-07-19 11:29:49 +0200

    add flexible list of annualized returns and always since INCEPTION

--- coinfolio_quant/datalake/backtest.py
+++ coinfolio_quant/datalake/backtest.py
@@ -184,6 +184,7 @@ def get_total_returns_table(database, strategy_ticker):
     total_values_series = get_performance_total_value_series(
         database, strategy_ticker)
 
+    first_timestamp_index = total_values_series.index[0]
     last_timestamp_index = total_values_series.index[-1]
 
     start_date_QTD_datetime = get_first_day_of_the_quarter(
@@ -205,6 +206,28 @@ def get_total_returns_table(database, strategy_ticker):
     annualized_return_1Y = pmetrics.annualized_return(
         total_values_series.loc[start_date_YTD_timestamp:], ann_factor=365)
 
+    # years_back_to_try = [1, 3, 5, 10]
+    years_back_to_try = [1, 2, 3, 5, 10]
+    annualized_returns_list = []
+    for year_back in years_back_to_try:
+        year_back_timestamp = pd.Timestamp(get_years_back_datetime(
+            last_timestamp_index.to_pydatetime(), year_back))
+        if first_timestamp_index < year_back_timestamp:
+            annualized_returns_list.append({
+                "label": f'{year_back}Y',
+                "start_date": year_back_timestamp,
+                "end_date": last_timestamp_index,
+                "percentage_return": pmetrics.annualized_return(
+                    total_values_series.loc[year_back_timestamp:], ann_factor=365)})
+
+    # always add since inception
+    annualized_returns_list.append({
+        "label": "INCEPTION",
+        "start_date": first_timestamp_index,
+        "end_date": last_timestamp_index,
+        "percentage_return": pmetrics.annualized_return(
+            total_values_series, ann_factor=365)})
+
     return {
         "cumulative_returns": [
             {"label": "QTD", "start_date": start_date_QTD_timestamp,
@@ -212,8 +235,9 @@ def get_total_returns_table(database, strategy_ticker):
             {"label": "YTD", "start_date": start_date_YTD_timestamp,
                 "end_date": last_timestamp_index, "percentage_return": cumulative_return_YTD},
         ],
-        "annualized_returns": [
-            {"label": "1Y", "start_date": start_date_1Y_timestamp,
-                "end_date": last_timestamp_index, "percentage_return": annualized_return_1Y},
-        ]
+        # "annualized_returns": [
+        #     {"label": "1Y", "start_date": start_date_1Y_timestamp,
+        #         "end_date": last_timestamp_index, "percentage_return": annualized_return_1Y},
+        # ]
+        "annualized_returns": annualized_returns_list
     }
